69b023ce4f5c81bfa6290a87949bb36d1458c9a2
Wellington-wsn <fogosark@hotmail.com>
2024-10-11 11:25:08 -0300

    Filtragem de moedas

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,9 @@ import execption.CustomApiException;
 import http.ApiCliente;
 import model.ConverterMoeda;
 import service.ConverterMoedaApi;
+import service.FiltroDeMoedas;
 
+import java.util.Map;
 import java.util.Scanner;
 
 
@@ -14,6 +16,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ApiCliente api = new ApiCliente();
+        FiltroDeMoedas filtro = new FiltroDeMoedas();
         Gson gson = new Gson();
 
         System.out.println("Digite a moeda para busca.");
@@ -26,6 +29,10 @@ public class Main {
             ConverterMoedaApi moedaApi = gson.fromJson(jsonResposta, ConverterMoedaApi.class);
             ConverterMoeda convertido = new ConverterMoeda(moedaApi);
             System.out.println(convertido);
+
+            Map<String, Double> moedasFiltradas = filtro.filtrarMoedas(convertido.getTaxaConversao());
+            System.out.println(moedasFiltradas);
+
 
         } catch (CustomApiException | JsonSyntaxException | JsonIOException e) {
             System.out.println("Erro ao converter JSON: " + e.getMessage());
---
+++ src/service/FiltroDeMoedas.java
@@ -0,0 +1,18 @@
+package service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class FiltroDeMoedas {
+    private static final List<String> listaDeMoedas = Arrays.asList("ARS", "BOB", "BRL", "CLP", "COP", "USD");
+
+    public Map<String, Double> filtrarMoedas(Map<String, Double> listaDeMoedasApi){
+        return listaDeMoedasApi
+                .entrySet()
+                .stream()
+                .filter(entrada -> listaDeMoedas.contains(entrada.getKey()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    };
+}
