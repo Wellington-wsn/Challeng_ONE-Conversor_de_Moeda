47b653c3d068a2fede10614fc2c5d067b45bda82
Wellington-wsn <fogosark@hotmail.com>
2024-10-11 14:45:15 -0300

    Convertendo valores

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonSyntaxException;
 import execption.CustomApiException;
 import http.ApiCliente;
 import model.ConverterMoeda;
+import service.CalcularTaxas;
 import service.ConverterMoedaApi;
 import service.FiltroDeMoedas;
 
@@ -21,18 +22,27 @@ public class Main {
 
         System.out.println("Digite a moeda para busca.");
         String pesquisarMoeda = sc.next();
-        sc.close();
 
         try {
             String jsonResposta = api.busca(pesquisarMoeda);
 
             ConverterMoedaApi moedaApi = gson.fromJson(jsonResposta, ConverterMoedaApi.class);
+            CalcularTaxas calcularTaxas = new CalcularTaxas(moedaApi);
             ConverterMoeda convertido = new ConverterMoeda(moedaApi);
             System.out.println(convertido);
 
             Map<String, Double> moedasFiltradas = filtro.filtrarMoedas(convertido.getTaxaConversao());
-            System.out.println(moedasFiltradas);
 
+
+            System.out.println("Escolha moeda para conversão:");
+            System.out.println(moedasFiltradas);
+            String moedaSelecionada = filtro.selecionarMoeda(moedasFiltradas, sc.next().toUpperCase());
+
+            System.out.println("Digite o valor para calcular a taxa da moeda:");
+            double valorMonetario = sc.nextDouble();
+            sc.close();
+            double valorConvertido = calcularTaxas.calcularConversao(moedaSelecionada, valorMonetario);
+            System.out.println("Valor convertido: " + valorConvertido);
 
         } catch (CustomApiException | JsonSyntaxException | JsonIOException e) {
             System.out.println("Erro ao converter JSON: " + e.getMessage());
--- src/service/FiltroDeMoedas.java
+++ src/service/FiltroDeMoedas.java
@@ -15,4 +15,12 @@ public class FiltroDeMoedas {
                 .filter(entrada -> listaDeMoedas.contains(entrada.getKey()))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
+
+    public String selecionarMoeda(Map<String, Double> moedasFiltradas, String moedaSelecionada) {
+        if (moedasFiltradas.containsKey(moedaSelecionada)) {
+            return moedaSelecionada;
+        } else {
+            throw new IllegalArgumentException("Moeda não permitida ou não encontrada: " + moedaSelecionada);
+        }
+    }
 }
